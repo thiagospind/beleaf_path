ae5a367eb42f8e5b0e50424f978835253173182c
Thiago Spindola <thiago.spindola@aapvr.com.br>
2019-07-02 18:06:47 -0300

    beleaf path

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,14 +2,14 @@ var mysql = require('mysql');
 
 var connMysql = function(){
     return mysql.createConnection({
-        // host: 'us-cdbr-iron-east-02.cleardb.net',
-        // user: 'b9f99e3c431ba4',
-        // password: '4aaab0cd',
-        // database: 'heroku_1bcba7123abcd1f'
-        host: 'localhost',
-        user: 'root',
-        password: 'aapvr',
-        database: 'beleaf'
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'b9f99e3c431ba4',
+        password: '4aaab0cd',
+        database: 'heroku_1bcba7123abcd1f'
+        // host: 'localhost',
+        // user: 'root',
+        // password: 'aapvr',
+        // database: 'beleaf'
     });
 }
 
